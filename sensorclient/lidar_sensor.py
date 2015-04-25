43057254330ce381c2d97a4b2555f086bf3340cd
mrreload <marc@thehoaglins.com>
2015-04-24 23:25:48 -0700

    starting on sensor input from pi, lidarlite

--- sensorclient/lidar_sensor.py
+++ sensorclient/lidar_sensor.py
@@ -1,37 +1,6 @@
 #!/usr/bin/python
 __author__ = 'marc.hoaglin'
-# int lidar_read(int fd) {
-#            int hiVal, loVal, i=0;
-#
-#            // send "measure" command
-#            hiVal = wiringPiI2CWriteReg8(fd, MEASURE_REG, MEASURE_VAL);
-#            if (_dbg) printf("write res=%d\n", hiVal);
-#            delay(20);
-#
-#            // Read second byte and append with first
-#            loVal = _read_byteNZ(fd, DISTANCE_REG_LO) ;
-#            if (_dbg) printf(" Lo=%d\n", loVal);
-#
-#            // read first byte
-#            hiVal = _read_byte(fd, DISTANCE_REG_HI) ;
-#            if (_dbg) printf ("Hi=%d ", hiVal);
-#
-#            return ( (hiVal << 8) + loVal);
-#     }
-# int lidar_init(bool dbg) {
-#             int fd;
-#             _dbg = dbg;
-#             if (_dbg) printf("LidarLite V0.1\n\n");
-#             fd = wiringPiI2CSetup(LIDAR_LITE_ADRS);
-#             if (fd != -1) {
-#                 lidar_status(fd);  // Dummy request to wake up device
-#                 delay (100);
-#                 }
-#             return(fd);
-#             }
-# unsigned char lidar_status(int fd) {
-#             return( (unsigned char) wiringPiI2CReadReg8(fd, STATUS_REG) );
-#             }
+
 from Adafruit_I2C import Adafruit_I2C
 from time import sleep
 i2c_address = 0x62
@@ -70,8 +39,6 @@ class Lidar_Lite(Adafruit_I2C):
 		return ((hival << 8) + loval)
 
 
-l = Lidar_Lite(i2c_address)
-print l.read_sensor()
 
 
 
--- sensorclient/sensor_init.py
+++ sensorclient/sensor_init.py
@@ -1,6 +1,8 @@
 __author__ = 'mrreload'
 import shlex, sys, subprocess
+from time import sleep
 schat = __import__('sensor_chat')
+sensor = __import__('lidar_sensor')
 
 class SensorMain(object):
 	def __init__(self):
@@ -8,8 +10,10 @@ class SensorMain(object):
 		execfile("sensor.conf", config)
 		args = config["sensor_command"]
 		self.cmd = shlex.split(args)
+		self.saddr = config["i2c_address"]
 		self.sc = schat.sensor_chat()
 		self.sc.connecttoserver()
+
 
 	def run_cmd(self):
 		result = "hello"
@@ -19,14 +23,22 @@ class SensorMain(object):
 			print "Unexpected error:", sys.exc_info()[0]
 		return result
 
+	def get_sensor_data(self):
+		sens = sensor.Lidar_Lite(self.saddr)
+		return sensor.read_sensor()
+
 	def snd_msg(self, msg):
 		self.sc.sendcommand(msg)
 
 
 p = SensorMain()
-output = p.run_cmd()
-p.snd_msg(output)
-print("output: " + output)
+# output = p.run_cmd()
+# p.snd_msg(output)
+# print("output: " + output)
+
+while True:
+	sleep(1)
+	p.snd_msg(p.get_sensor_data())
 
 
 
