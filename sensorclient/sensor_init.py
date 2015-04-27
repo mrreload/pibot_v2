bbab345365ca961f1ebac332384396e01c3119e9
Marc Hoaglin <hoaglin69@gmail.com>
2015-04-27 13:54:12 -0700

    adding server files

--- sensorclient/sensor_init.py
+++ sensorclient/sensor_init.py
@@ -35,7 +35,7 @@ class SensorMain(object):
 			self.snd_msg(self.sens2.get_bearing())
 
 	def snd_msg(self, msg):
-		self.sc.send_data()
+		self.sc.send_data(msg)
 
 	def spawn_threads(self):
 		t1 = threading.Thread(name="lidar", target=self.lidar)
