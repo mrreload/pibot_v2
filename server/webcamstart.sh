9b7f18e5c5471263c31e1a07f7689237c1293bdb
Jonathon Hoaglin <jonhoaglin@gmail.com>
2015-04-28 15:02:13 -0700

    Update webcamstart.sh

--- server/webcamstart.sh
+++ server/webcamstart.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=/usr/local/lib
-sleep 60; raspivid -t 0 -h 720 -w 1080 -fps 48 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
+sleep 15; raspivid -t 0 -h 720 -w 1080 -fps 48 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
