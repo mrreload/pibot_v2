a98c70ccd3c0e42808144460746f42a78fbf8223
Jonathon Hoaglin <jonhoaglin@gmail.com>
2015-05-05 10:37:52 -0700

    grid plotting methods for adding points to the 2d map area

--- client/master_control.py
+++ client/master_control.py
@@ -7,7 +7,6 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 import ConfigParser
 import geo
-from gi.repository import OsmGpsMap
 
 # Needed for window.get_xid(), xvimagesink.set_window_handle(), respectively:
 # from gi.repository import GdkX11, GstVideo
@@ -21,11 +20,12 @@ Empty = Queue.Empty
 class Player(object):
 
 	def __init__(self):
+		# Grab and parse the client.conf file
 		self.config = ConfigParser.ConfigParser()
-		#If the module is executed as a script __name__ will be '__main__' and sys.argv[0] will be the full path of the module.
+		# If the module is executed as a script __name__ will be '__main__' and sys.argv[0] will be the full path of the module.
 		if __name__ == '__main__':
 			path = os.path.split(sys.argv[0])[0]
-		#Else the module was imported and it has a __file__ attribute that will be the full path of the module.
+		# Else the module was imported and it has a __file__ attribute that will be the full path of the module.
 		else:
 			path = os.path.split(__file__)[0]
 		self.config.read(os.path.join(os.path.dirname(path), 'client.conf'))
@@ -34,11 +34,13 @@ class Player(object):
 			self.config.read(os.path.join(path, 'client.conf'))
 			with open(os.path.join(os.path.dirname(path), 'client.conf'), "w") as conf:
 				self.config.write(conf)
-		#set variables for host and port
+		# Set variables for host and port
 		self.host = self.config.get("Connection", "host")
 		self.video_port = int(self.config.get("Connection", "video_port"))
 		self.message_port = int(self.config.get("Connection", "message_port"))
-		#something for gps?
+		# Set the message queue
+		self.msg_q = Queue.Queue(maxsize=0)
+		# Something for gps?
 		my_lon = 33.6880290
 		my_lat = 117.9861210
 		hb = geo.xyz(my_lat, my_lon)
@@ -48,16 +50,10 @@ class Player(object):
 
 
 	def do_real_init(self):
-
-		self.msg_q = Queue.Queue(maxsize=0)
-
+		# Initialize the tkinter gui
 		self.showWindow()
-
 		# Keyboard bindings
 		self.setup_key_binds()
-
-		self.map = OsmGpsMap
-		print self.map.__dir__()
 		# Setup Messaging Connection
 		self.chat = ch.chat_client(self.host, self.message_port)
 		self.chat.connecttoserver()
@@ -73,34 +69,34 @@ class Player(object):
 		self.bus = self.pipeline.get_bus()
 
 	def showWindow(self):
-		#setup main client gui
+		# Setup main client gui
 		self.window = tk.Tk()
 		self.window.title("PiBot Control")
 		self.window.geometry('1280x590')
 		self.window.protocol("WM_DELETE_WINDOW", self.exithandler)
 		self.window.configure(background="black")
-		#configure grid
+		# Configure grid for stretchiness
 		self.window.columnconfigure(0, weight=1)
 		self.window.columnconfigure(1, weight=1)
 		self.window.columnconfigure(2, weight=1)
 		self.window.columnconfigure(3, weight=12)
-		self.window.rowconfigure(0, weight=10)
-		self.window.rowconfigure(1, weight=31)
+		self.window.rowconfigure(0, weight=6)
+		self.window.rowconfigure(1, weight=32)
 		self.window.rowconfigure(2, weight=1)
-		#add frames for display
+		# Add frames for display
 		self.compassFrame = tk.Frame(self.window, background="black")
 		self.compassFrame.grid(row=0,column=0, sticky="nsew", padx=1, pady=1)
 		self.lidarFrame = tk.Frame(self.window, background="black")
 		self.lidarFrame.grid(row=0,column=1, sticky="nsew", padx=1, pady=1)
 		self.gpsFrame = tk.Frame(self.window, background="black")
 		self.gpsFrame.grid(row=0,column=2, sticky="nsew", padx=1, pady=1)
-		self.mapFrame = tk.Frame(self.window, background="")
+		self.mapFrame = tk.Frame(self.window, bg="yellow")
 		self.mapFrame.grid(row=1,column=0,columnspan=3, sticky="nsew", padx=1, pady=1)
 		self.videoFrame = tk.Frame(self.window, bg="")
 		self.videoFrame.grid(row=0,column=3,rowspan=2, sticky="nsew", padx=1, pady=1)
 		self.statusFrame = tk.Frame(self.window, background="black")
 		self.statusFrame.grid(row=2,column=0,columnspan=4, sticky="nsew", padx=1, pady=1)
-		#add menu to gui
+		# Add menu to gui
 		self.menubar = tk.Menu(self.window)
 		self.window.config(menu=self.menubar)
 		self.fileMenu = tk.Menu(self.menubar)
@@ -108,13 +104,15 @@ class Player(object):
 		self.fileMenu.add_separator()
 		self.fileMenu.add_command(label="Exit", command=self.window.quit)
 		self.menubar.add_cascade(label="File", menu=self.fileMenu)
-		#initialize variables to update labels directly
+		# Initialize variables to update labels directly
 		self.compassValue = tk.StringVar()
 		self.compassValue.set("Compass!")
 		self.headingValue = tk.StringVar()
 		self.headingValue.set("Heading!")
-		self.lidarValue = tk.StringVar()
-		self.lidarValue.set("Lidar!")
+		self.lidarValue_cm = tk.StringVar()
+		self.lidarValue_cm.set("Lidar cm!")
+		self.lidarValue_in = tk.StringVar()
+		self.lidarValue_in.set("Lidar in!")
 		self.gpsValue = tk.StringVar()
 		self.gpsValue.set("GPS!")
 		self.statusValue = tk.StringVar()
@@ -124,14 +122,36 @@ class Player(object):
 		self.compassLabel.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)
 		self.headingLabel = tk.Label(self.compassFrame, textvariable=self.headingValue)
 		self.headingLabel.pack(side=tk.TOP, expand=tk.NO, fill=tk.BOTH)
-		self.lidarLabel = tk.Label(self.lidarFrame, textvariable=self.lidarValue)
-		self.lidarLabel.pack(expand=tk.YES, fill=tk.BOTH)
+		self.lidarLabel_cm = tk.Label(self.lidarFrame, textvariable=self.lidarValue_cm)
+		self.lidarLabel_cm.pack(expand=tk.YES, fill=tk.BOTH)
+		self.lidarLabel_in = tk.Label(self.lidarFrame, textvariable=self.lidarValue_in)
+		self.lidarLabel_in.pack(expand=tk.NO, fill=tk.BOTH)
 		self.gpsLabel = tk.Label(self.gpsFrame, textvariable=self.gpsValue)
 		self.gpsLabel.pack(expand=tk.YES, fill=tk.BOTH)
 		self.statusLabel = tk.Label(self.statusFrame, textvariable=self.statusValue)
 		self.statusLabel.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH)
+		# Pack a canvas into the map frame for plotting points
+		self.mapCanvas = tk.Canvas(self.mapFrame, bg="white", width=1, height=1)
+		self.mapCanvas.pack(expand=tk.YES, fill=tk.BOTH)
+		self.mapCanvas.bind("<Configure>", self.mapresize)
 		#set focus to video frame
 		self.window.focus_set()
+
+	def mapresize(self, event):
+		self.mapCanvas.delete("all")
+		self.mapCanvas_width, self.mapCanvas_height = event.width, event.height
+		# Draw x-axis
+		self.mapCanvas.create_line(0, self.mapCanvas_height/2, self.mapCanvas_width, self.mapCanvas_height/2, dash=2, arrow=tk.BOTH)
+		# Draw y-axis
+		self.mapCanvas.create_line(self.mapCanvas_width/2, 0, self.mapCanvas_width/2, self.mapCanvas_height, dash=2, arrow=tk.BOTH)
+		self.plotpoint(5, 5)
+
+	def plotpoint(self, x_center, y_center):
+		# Convert from grid coords to real canvas coords
+		x_center = (self.mapCanvas_width/2) + x_center
+		y_center = (self.mapCanvas_height/2) - y_center
+		# Plot the point as a circle centered on the coords given
+		self.mapCanvas.create_oval(x_center-1, y_center-1, x_center+1, y_center+1, fill="red")
 
 
 	def showConfig(self):
@@ -315,7 +335,8 @@ class Player(object):
 					sn = cmd.split(',')
 					if sn[0] == "Sensor":
 						if sn[1] == "Lidar":
-							self.lidarValue.set(float(sn[2])*0.39370)
+							self.lidarValue_cm.set(sn[2]+" cm")
+							self.lidarValue.set(str(float(sn[2])*0.39370)+" in")
 						if sn[1] == "Compass":
 							self.compassValue.set(geo.direction_name(float(sn[2])))
 							self.headingValue.set(float("{0:.2f}".format(float(sn[2]))))
