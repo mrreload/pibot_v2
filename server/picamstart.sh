bcd03ae051daae359395e4ec125617f9a4bc7878
mrreload <marc@thehoaglins.com>
2015-05-16 17:51:47 -0700

    compass test

--- server/picamstart.sh
+++ server/picamstart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 export LD_LIBRARY_PATH=/usr/local/lib
-sleep 15; raspivid -t 0 -h 720 -w 1280 -fps 48 -b 3000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
-export LD_LIBRARY_PATH=/usr/local/lib
-sleep 15; raspivid -t 0 -h 720 -w 1280 -fps 48 -b 3000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
+sleep 10; raspivid -t 0 -h 720 -w 1280 -fps 48 -b 3000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=0.0.0.0 port=5000
+
