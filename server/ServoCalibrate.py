87464fa456a0f905ff4a676bdc6b5431eed2dc73
Jonathon Hoaglin <jonhoaglin@gmail.com>
2015-06-24 11:20:13 -0700

    quick and dirty servo calibration method

--- server/ServoCalibrate.py
+++ server/ServoCalibrate.py
@@ -6,7 +6,7 @@ from ServoHATDriver import stdServo
 
 pwm = PWM(0X40)
 # stdServo(pwm, frequency, channel, servoRange, minPulse, maxPulse, debug=False)
-self.pan = stdServo(pwm, 50, 0, 180, 500, 2710)
-self.tilt = stdServo(pwm, 50, 3, 190, 500, 2710)
+pan = stdServo(pwm, 50, 0, 180, 500, 2710)
+tilt = stdServo(pwm, 50, 3, 190, 500, 2710)
 
-self.pan.calibrate()
+pan.calibrate()
